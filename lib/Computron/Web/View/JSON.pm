b1bd3c9f4ee487a33ab4739ae027f84a5b3ac987
Devin Austin <dhoss@cpan.org>
2011-12-15 17:40:48 -0700

    added JSON view

---
+++ lib/Computron/Web/View/JSON.pm
@@ -0,0 +1,29 @@
+package Computron::Web::View::JSON;
+
+use strict;
+use base 'Catalyst::View::JSON';
+
+=head1 NAME
+
+Computron::Web::View::JSON - Catalyst JSON View
+
+=head1 SYNOPSIS
+
+See L<Computron::Web>
+
+=head1 DESCRIPTION
+
+Catalyst JSON View.
+
+=head1 AUTHOR
+
+Devin Austin
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
