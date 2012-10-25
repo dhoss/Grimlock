948fa73a7bb41a5bdc787258864e4880ece148f7
Devin Austin <dhoss@cpan.org>
2012-10-25 10:42:37 -0700

    added session class

---
+++ Grimlock-App/lib/Grimlock/App/Schema/Result/Session.pm
@@ -0,0 +1,91 @@
+use utf8;
+package Grimlock::App::Schema::Result::Session;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Grimlock::App::Schema::Result::Session
+
+=cut
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::TimeStamp>
+
+=item * L<DBIx::Class::EncodedColumn>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp", "EncodedColumn");
+
+=head1 TABLE: C<sessions>
+
+=cut
+
+__PACKAGE__->table("sessions");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'char'
+  is_nullable: 0
+  size: 72
+
+=head2 session_data
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 expires
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "char", is_nullable => 0, size => 72 },
+  "session_data",
+  { data_type => "text", is_nullable => 1 },
+  "expires",
+  { data_type => "integer", is_nullable => 1 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+
+# Created by DBIx::Class::Schema::Loader v0.07033 @ 2012-10-25 10:42:20
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:4K5eqIlY0akSyC6BYmjbeQ
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->meta->make_immutable;
+1;
