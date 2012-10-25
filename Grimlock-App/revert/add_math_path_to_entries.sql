c25c66fa1db80641418fb98b51a492211c1fe2e3
Devin Austin <dhoss@cpan.org>
2012-10-25 13:31:11 -0700

    modified revert sql for matpath

--- Grimlock-App/revert/add_math_path_to_entries.sql
+++ Grimlock-App/revert/add_math_path_to_entries.sql
@@ -3,5 +3,9 @@
 BEGIN;
 
 -- XXX Add DDLs here.
-
+ALTER TABLE grimlock.entries DROP COLUMN parent_id;
+ALTER TABLE grimlock.entries DROP COLUMN parent_path;
 COMMIT;
+
+DROP INDEX IF EXISTS entry_parent_idx;
+DROP INDEX IF EXISTS entry_parent_path_idx;
